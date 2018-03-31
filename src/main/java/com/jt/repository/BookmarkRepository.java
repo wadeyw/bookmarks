07f68241190e965cc10454561410f6a8835b820e
wadec <wadezyw@gmail.com>
2018-03-31 19:35:24 +0800

    all action and CORS

---
+++ src/main/java/com/jt/repository/BookmarkRepository.java
@@ -0,0 +1,17 @@
+package com.jt.repository;
+
+
+import com.jt.model.Bookmark;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+
+@RepositoryRestResource
+public interface BookmarkRepository extends CrudRepository<Bookmark, Long> {
+    @RestResource(path="note")
+    List<Bookmark> findByNoteContaining(@Param("text")String note);
+}
